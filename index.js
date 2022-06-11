766b05bd265bc250fbe984c99f5cc10267159f4e
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-06-11 15:57:51 -0700

    modified index.js and moved documentation.html

--- index.js
+++ index.js
@@ -1,3 +1,89 @@
 const express = require("express");
 morgan = require("morgan");
+fs = require("fs"); // import node modules fs and path
+path = require("path");
+
 const app = express();
+//create a write stream(in append mode)
+//a 'log.txt' file is created in root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
+  flags: "a"
+});
+
+let movies = [
+  {
+    title: "The Godfather",
+    director: "Francis For Coppola"
+  },
+  {
+    title: "Top Gun",
+    director: "Tony Scott"
+  },
+  {
+    title: "Get Out",
+    director: "Jordan Peele"
+  },
+  {
+    title: "Jurassic Park",
+    director: "Steven Spielberg"
+  },
+  {
+    title: "Spider-man: No Way Home",
+    director: "Jon Watts"
+  },
+  {
+    title: "The Batman",
+    director: "Matt Reeves"
+  },
+  {
+    title: "The Avengers",
+    director: "Joss Whedon"
+  },
+  {
+    title: "Avatar",
+    director: "James Cameron"
+  },
+  {
+    title: "Independence Day",
+    director: "Roland Emmerich"
+  },
+  {
+    title: "Good Will Hunting",
+    director: "Gus Van Sant"
+  }
+];
+
+//GET request
+// GET requests
+app.get("/", (req, res) => {
+  res.send("Welcome to myFlix!");
+});
+
+app.get("/documentation", (req, res) => {
+  res.sendFile("public/documentation.html", {
+    root: __dirname
+  });
+});
+
+app.get("/movies", (req, res) => {
+  res.json(movies);
+});
+
+//Middleware functions
+app.use(
+  morgan("combined", {
+    stream: accessLogStream
+  })
+);
+
+app.use(express.static("public"));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+//listen for request
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
+});
