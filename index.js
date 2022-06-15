dc7375d65eb94e873297c48bb05ac5d87258cf64
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-06-15 16:20:13 -0700

    add GET director

--- index.js
+++ index.js
@@ -31,52 +31,92 @@ app.use(bodyParser.json());
 let movies = [{
         "Title": "The Godfather",
         "Genre": "Thriller",
-        "Director": "Francis For Coppola"
+        "Director": {
+            "Name": "Francis For Coppola",
+            "Bio": "American film director, producer, and screenwriter. He is considered one of the major figures of the New Hollywood filmmaking movement of the 1960s and 1970s.",
+            "Birth Year": 1939
+        }
     },
     {
         "Title": "Top Gun",
         "Genre": "Action",
-        "Director": "Tony Scott"
+        "Director": {
+            "Name": "Tony Scott",
+            "Bio": "British film director and producer. He was known for directing highly successful action and thriller films such as Top Gun (1986), Beverly Hills Cop II (1987), Days of Thunder (1990), The Last Boy Scout (1991), True Romance (1993), Crimson Tide (1995), Enemy of the State (1998), Man on Fire (2004), Déjà Vu (2006), and Unstoppable (2010).",
+            "Birth Year": 1944
+        }
     },
     {
         "Title": "Get Out",
         "Genre": "Suspense/Horror",
-        "Director": "Jordan Peele"
+        "Director": {
+            "Name": "Jordan Peele",
+            "Bio": "American actor, comedian and filmmaker. He is best known for his film and television work in the comedy and horror genres.",
+            "Birth Year": 1979
+        }
     },
     {
         "Title": "Jurassic Park",
         "Genre": "Thriller/Action",
-        "Director": "Steven Spielberg"
+        "Director": {
+            "Name": "Steven Spielberg",
+            "Bio": "American film director, producer, and screenwriter.",
+            "Birth Year": 1946
+        }
     },
     {
         "Title": "Spider-man: No Way Home",
         "Genre": "Action",
-        "Director": "Jon Watts"
+        "Director": {
+            "Name": "Jon Watts",
+            "Bio": "American filmmaker. His credits include directing the Marvel Cinematic Universe (MCU) superhero films Spider-Man: Homecoming, Spider-Man: Far From Home, and Spider-Man: No Way Home.",
+            "Birth Year": 1981
+        }
     },
     {
         "Title": "The Batman",
         "Genre": "Action",
-        "Director": "Matt Reeves"
+        "Director": {
+            "Name": "Matt Reeves",
+            "Bio": "American film director, producer and screenwriter.",
+            "Birth Year": 1966
+        }
     },
     {
         "Title": "The Avengers",
         "Genre": "Action",
-        "Director": "Joss Whedon"
+        "Director": {
+            "Name": "Joss Whedon",
+            "Bio": "American filmmaker, composer, and comic book writer.",
+            "Birth Year": 1964
+        }
     },
     {
         "Title": "Avatar",
         "Genre": "Sci-Fi",
-        "Director": "James Cameron"
+        "Director": {
+            "Name": "James Cameron",
+            "Bio": "Canadian filmmaker. Best known for making science fiction and epic films, he first gained recognition for directing The Terminator.",
+            "Birth Year": 1954
+        }
     },
     {
         "Title": "Independence Day",
         "Genre": "Sci-Fi",
-        "Director": "Roland Emmerich"
+        "Director": {
+            "Name": "Roland Emmerich",
+            "Bio": "German film director, screenwriter, and producer. He is widely known for his science fiction and disaster films and has been called a 'master of disaster' within the industry.",
+            "Birth Year": 1955
+        }
     },
     {
         "Title": "Good Will Hunting",
         "Genre": "Drama",
-        "Director": "Gus Van Sant"
+        "Director": {
+            "Name": "Gus Van Sant",
+            "Bio": "American film director, producer, photographer, and musician who has earned acclaim as both an independent and mainstream filmmaker.",
+            "Birth Year": 1952
+        }
     }
 ];
 
@@ -121,6 +161,20 @@ app.get("/movies/genre/:genreName", (req, res) => {
     }
 
 });
+
+//READ
+app.get("/movies/director/:directorName", (req, res) => {
+    const { directorName } = req.params;
+    const director = movies.find(movie => movie.Director.Name === directorName).Director;
+
+    if (director) {
+        res.status(200).json(director);
+    } else {
+        res.status(400).send('no such director')
+    }
+
+});
+
 
 
 
