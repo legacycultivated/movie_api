cd8ec28dd03aa23eb66c7c9b1e73aaa78ace8a9d
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-06-15 21:11:43 -0700

    updated CRUD actions in index.js

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const app = express();
 
 app.use(bodyParser.json());
 
+let users = [{
+        id: 1,
+        name: "Kim",
+        favoriteMovies: []
+    },
+    {
+        id: 2,
+        name: "Joe",
+        favoriteMovies: ["The Godfather"]
+    }
+];
+
+
 let movies = [{
         "Title": "The Godfather",
         "Genre": "Thriller",
@@ -100,6 +113,23 @@ let movies = [{
     }
 ];
 
+
+//Add New Users
+// CREATE
+app.post("/users", (req, res) => {
+    const newUser = req.body;
+    if (newUser.name) {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(201).json(newUser)
+    } else {
+        res.status(400).send("users need names")
+    }
+
+});
+
+
+
 //GET request
 app.get("/", (req, res) => {
     res.send("Welcome to myFlix!");
@@ -150,8 +180,70 @@ app.get("/movies/director/:directorName", (req, res) => {
 
 });
 
+//UPDATE
+app.put("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const updatedUser = req.body;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        user.name = updatedUser.name;
+        res.status(200).json(user);
+    } else {
+        res.status(400).send("no such user")
+    }
+
+});
 
 
+//UPDATE or POST
+app.post("/users/:id/:movieTitle", (req, res) => {
+    const { id, movieTitle } = req.params;
+    const updatedUser = req.body;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        user.favoriteMovies.push(movieTitle);
+        res.status(200).send(`${movieTitle} has been added to user ${id}'s array.`);
+    } else {
+        res.status(400).send("no such user")
+    }
+
+});
+
+// DELETE
+app.delete("/users/:id/:movieTitle", (req, res) => {
+    const { id, movieTitle } = req.params;
+
+    let user = users.find(user => user.id == id);
+
+
+    if (user) {
+        user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
+        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array.`);
+    } else {
+        res.status(400).send("no such user")
+    }
+
+});
+
+// DELETE
+app.delete("/users/:id", (req, res) => {
+    const { id } = req.params;
+
+    let user = users.find(user => user.id == id);
+
+
+    if (user) {
+        users = users.filter(user => user.id != id);
+        res.status(200).send(`user ${id} has been deleted.`);
+    } else {
+        res.status(400).send("no such user")
+    }
+
+});
 
 
 //Middleware functions
