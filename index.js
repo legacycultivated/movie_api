7d8970e04c9204713b1748a7d27cb75bd701c86a
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-06-21 14:28:19 -0700

    organize index.js

--- index.js
+++ index.js
@@ -20,7 +20,86 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+//GET request
+app.get("/", (req, res) => {
+    res.send("Welcome to myFlix!");
+});
 
+//GET All Movies
+app.get("/movies", (req, res) => {
+    Movies.find()
+        .then((movies) => {
+            res.status(201).json(movies);
+        })
+        .catch((err) => {
+            console.err(err);
+            res.status(500).send("Error: " + err);
+        });
+});
+
+//GET Movies by Title
+app.get('/movies/:Title', (req, res) => {
+    Movies.findOne({ Title: req.params.Title })
+        .then((movie) => {
+            res.json(movie);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error: " + err);
+        });
+
+
+});
+
+
+//GET Genre info
+app.get("/movies/genre/:Name", (req, res) => {
+    Movies.findOne({ 'Genre.Name': req.params.Name })
+        .then((genre) => {
+            res.json(genre.Description);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send('Error: ' + err);
+        });
+
+});
+
+//GET Directors info
+app.get("/movies/director/:Name", (req, res) => {
+    Movies.findOne({ 'Director.Name': req.params.Name })
+        .then((director) => {
+            res.json(director);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
+//GET all users
+app.get('/users', (req, res) => {
+    Users.find()
+        .then((users) => {
+            res.status(201).json(users);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
+//GET user by username
+app.get('/users/:Username', (req, res) => {
+    Users.findOne({ Username: req.params.Username })
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
 
 //Add New Users
@@ -51,97 +130,8 @@ app.post('/users', (req, res) => {
         });
 });
 
-//GET all users
-app.get('/users', (req, res) => {
-    Users.find()
-        .then((users) => {
-            res.status(201).json(users);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send('Error: ' + err);
-        });
-});
-
-//GET user by username
-app.get('/users/:Username', (req, res) => {
-    Users.findOne({ Username: req.params.Username })
-        .then((user) => {
-            res.json(user);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send('Error: ' + err);
-        });
-});
 
 
-
-//GET request
-app.get("/", (req, res) => {
-    res.send("Welcome to myFlix!");
-});
-
-
-//READ
-app.get("/movies", (req, res) => {
-    res.status(200).json(movies);
-});
-
-//READ
-app.get("/movies/title/:titleName", (req, res) => {
-    const { titleName } = req.params;
-    const title = movies.find(movie => movie.Title === titleName);
-
-    if (title) {
-        res.status(200).json(title);
-    } else {
-        res.status(400).send('no such movie')
-    }
-
-});
-
-//READ
-app.get("/movies/genre/:genreName", (req, res) => {
-    const { genreName } = req.params;
-    const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
-
-    if (genre) {
-        res.status(200).json(genre);
-    } else {
-        res.status(400).send('no such genre')
-    }
-
-});
-
-//READ
-app.get("/movies/director/:directorName", (req, res) => {
-    const { directorName } = req.params;
-    const director = movies.find(movie => movie.Director.Name === directorName).Director;
-
-    if (director) {
-        res.status(200).json(director);
-    } else {
-        res.status(400).send('no such director')
-    }
-
-});
-
-//UPDATE
-app.put("/users/:id", (req, res) => {
-    const { id } = req.params;
-    const updatedUser = req.body;
-
-    let user = users.find(user => user.id == id);
-
-    if (user) {
-        user.name = updatedUser.name;
-        res.status(200).json(user);
-    } else {
-        res.status(400).send("no such user")
-    }
-
-});
 
 //UPDATE user info by Username
 app.put('/users/:Username', (req, res) => {
