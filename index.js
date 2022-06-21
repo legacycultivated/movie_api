14984e674e468b2bbc640a580e3aa0047760e868
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-06-21 12:53:43 -0700

    added Mongoose Queries

--- index.js
+++ index.js
@@ -212,6 +212,22 @@ app.delete("/users/:id/:movieTitle", (req, res) => {
 
 });
 
+//DELETE a user by username
+app.delete('/users/:Username', (req, res) => {
+    Users.findOneAndRemove({ Username: req.params.Username })
+        .then((user) => {
+            if (!user) {
+                res.status(400).send(req.params.Username + ' was not found');
+            } else {
+                res.status(200).send(req.params.Username + ' was deleted.');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
 // DELETE
 app.delete("/users/:id", (req, res) => {
     const { id } = req.params;
